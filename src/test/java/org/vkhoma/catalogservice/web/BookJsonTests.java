e8cbb0e71bcf9627759af49d704e9acc77ac4870
vkhoma <16196348+HVV83@users.noreply.github.com>
2025-08-07 14:20:38 +0200

    feat: add auditing fields to Book entity and enable JDBC auditing

--- src/test/java/org/vkhoma/catalogservice/web/BookJsonTests.java
+++ src/test/java/org/vkhoma/catalogservice/web/BookJsonTests.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
@@ -16,7 +19,8 @@ class BookJsonTests {
 
     @Test
     void testSerialize() throws Exception {
-        var book = new Book(123L, "1234567890", "Title", "Author", 9.90, 1);
+        var instant = Instant.now();
+        var book = new Book(123L, "1234567890", "Title", "Author", 9.90, instant, instant, 1);
         var jsonContent = json.write(book);
 
         assertThat(jsonContent).extractingJsonPathNumberValue("@.id")
@@ -29,12 +33,17 @@ class BookJsonTests {
                 .isEqualTo(book.author());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
                 .isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.createdDate")
+                .isEqualTo(book.createdDate().toString());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.lastModifiedDate")
+                .isEqualTo(book.lastModifiedDate().toString());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.version")
                 .isEqualTo(book.version());
     }
 
     @Test
     void testDeserialize() throws Exception {
+        var instant = Instant.parse("2025-08-07T00:00:00.000000Z");
         var content = """
                 {
                     "id": 123,
@@ -42,12 +51,14 @@ class BookJsonTests {
                     "title": "Title",
                     "author": "Author",
                     "price": 9.90,
+                    "createdDate": "2025-08-07T00:00:00.000000Z",
+                    "lastModifiedDate": "2025-08-07T00:00:00.000000Z",
                     "version": 1
                 }
                 """;
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(new Book(123L,"1234567890", "Title", "Author", 9.90, 1));
+                .isEqualTo(new Book(123L,"1234567890", "Title", "Author", 9.90, instant, instant, 1));
     }
 
 }
